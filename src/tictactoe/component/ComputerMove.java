4cbef5cb77cc7b8630e472e9cd34369c946f7099
5taY3r <ws.granta@gmail.com>
2023-02-01 17:13:29 +0600

    Final

--- src/tictactoe/component/ComputerMove.java
+++ src/tictactoe/component/ComputerMove.java
@@ -16,7 +16,10 @@
 
 package tictactoe.component;
 
+import tictactoe.model.Cell;
 import tictactoe.model.GameTable;
+
+import java.util.Random;
 
 /**
  * 5taY3r
@@ -24,6 +27,15 @@ import tictactoe.model.GameTable;
  */
 public class ComputerMove {
     public void make(final GameTable gameTable) {
-
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, '0');
+                break;
+            }
+        }
     }
 }
