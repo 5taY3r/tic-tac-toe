28cee0de34b458fe6512a63cbf8120ce5c95c584
5taY3r <ws.granta@gmail.com>
2023-02-01 12:04:12 +0600

    Implement DataPrinter component

--- src/tictactoe/component/DataPrinter.java
+++ src/tictactoe/component/DataPrinter.java
@@ -16,6 +16,7 @@
 
 package tictactoe.component;
 
+import tictactoe.model.Cell;
 import tictactoe.model.GameTable;
 
 /**
@@ -23,11 +24,59 @@ import tictactoe.model.GameTable;
  * allinternet.io
  */
 public class DataPrinter {
-    public void printMappingTable() {
+    /*
+-------------
+| 7 | 8 | 9 |
+-------------
+| 4 | 5 | 6 |
+-------------
+| 1 | 2 | 3 |
+-------------
 
+-------------
+|   |   |   |
+-------------
+|   | X |   |
+-------------
+|   |   | O |
+-------------
+     */
+
+    public void printMappingTable() {
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
     public void printGameTable(final GameTable gameTable) {
+      /*System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(0, 0)) + " | " +
+                gameTable.getSign(new Cell(0, 1)) + " | " +
+                gameTable.getSign(new Cell(0, 2)) + " |");
+        System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(1, 0)) + " | " +
+                gameTable.getSign(new Cell(1, 1)) + " | " +
+                gameTable.getSign(new Cell(1, 2)) + " |");
+        System.out.println("-------------");
+        System.out.println("| " +
+                gameTable.getSign(new Cell(2, 0)) + " | " +
+                gameTable.getSign(new Cell(2, 1)) + " | " +
+                gameTable.getSign(new Cell(2, 2)) + " |");
+        System.out.println("-------------");*/
 
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
